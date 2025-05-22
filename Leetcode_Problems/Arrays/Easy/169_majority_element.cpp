dd9df27f21a35170818b91c5f0ae43549ead973b
Umee <umeshmessi23@gmail.com>
2025-05-22 06:37:31 +0530

    Array: optimized majority element problem

--- Leetcode_Problems/Arrays/Easy/169_majority_element.cpp
+++ Leetcode_Problems/Arrays/Easy/169_majority_element.cpp
@@ -6,18 +6,30 @@ class Solution {
         int majorityElement(vector<int>& nums) {
 
             // My Bruteforce
-            unordered_map<int, int> elements;
-            for(int num : nums) elements[num]++;
+            // unordered_map<int, int> elements;
+            // for(int num : nums) elements[num]++;
             
-            int majorityElement = -1;
-            for(auto element : elements) {
-                if(element.second > nums.size() / 2) {
-                    majorityElement = element.first;
-                    return majorityElement;
-                }
-            }
-            return majorityElement;
+            // int majorityElement = -1;
+            // for(auto element : elements) {
+            //     if(element.second > nums.size() / 2) {
+            //         majorityElement = element.first;
+            //         return majorityElement;
+            //     }
+            // }
+            // return majorityElement;
             // Time Complexity - O(n) Space Complexity - O(n)
-            // Optimal one exists with Booyer-Moore voting algorithm with Space Complexity - O(1)
+
+            // Optimal 
+            // Boyer-Moore Algorithm
+            int votes = 0;
+            int candidate = 0;
+            for(int i = 0; i < nums.size(); i++) {
+                if(votes == 0) {
+                    candidate = nums[i];
+                }
+                votes += ((candidate == nums[i]) ? 1 : -1);
+            }
+            return candidate;
+
         }
     };

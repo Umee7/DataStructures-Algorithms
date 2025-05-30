46669f390cc1bb183598df451a8044251611cac8
Umee <umeshmessi23@gmail.com>
2025-05-30 07:11:00 +0530

    Algorithm: solved lowerbound problem using Binary Search

---
+++ Algorithms/BinarySearchOnAnswers/lower_bound.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public: 
+    int lowerBound(vector<int> &nums, int key) {
+        int low = 0, high = nums.size() - 1;
+        int result = nums.size();
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] >= key) {
+                result = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+	Solution solution;
+	vector<int> nums = {1,2,2,3};
+    int key = 0;
+    int result = solution.lowerBound(nums, key);
+    cout<<result;
+}

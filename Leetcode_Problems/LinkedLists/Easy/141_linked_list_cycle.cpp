58b18923bb1d66e58c393bca8193e4289bd9ff98
Umee <umeshmessi23@gmail.com>
2025-06-09 16:51:00 +0530

    Linked List: optimized linked list cycle problem

--- Leetcode_Problems/LinkedLists/Easy/141_linked_list_cycle.cpp
+++ Leetcode_Problems/LinkedLists/Easy/141_linked_list_cycle.cpp
@@ -11,18 +11,29 @@ class Solution {
 public:
    bool hasCycle(ListNode* head) {
     // Brute Force
-       unordered_map<ListNode*, int> map;
-       ListNode* temp = head;
-       while (temp != nullptr) {
+    //    unordered_map<ListNode*, int> map;
+    //    ListNode* temp = head;
+    //    while (temp != nullptr) {
+    //        if (map.find(temp) != map.end()) {
+    //            return true;
+    //        }
+    //        map[temp] = 1;
+    //        temp = temp->next;
+    //    }
+    //    return false;
 
-           if (map.find(temp) != map.end()) {
-               return true;
-           }
-
-           map[temp] = 1;
-
-           temp = temp->next;
-       }
-       return false;
+    // My Optimal
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast!=nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    // Time Complexity - O(n) Space Complexity - O(1)
+    // Note: compare nodes not their values
    }
 };
